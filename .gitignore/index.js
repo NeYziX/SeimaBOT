d30bef340daad1f36e0bd001a54f59ede12c21e8
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-28 11:23:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -122,6 +122,6 @@ bot.on("guildMemberRemove", member => {
 });
 
 bot.on("guildMemberAdd", member => {
-    var role = member.guild.roles.find('name', 'Nouveaux');
+    var role = member.guild.roles.find('name', 'Membres');
     member.addRole(role)
 })
