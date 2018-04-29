3a52b6a9c8fde010bbb7df2ab1a52dc5088ccebd
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-04-29 21:23:50 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const FileSync = require
 
 var prefix = ("&");
 
@@ -165,11 +166,11 @@ bot.on('message', message => {
 });
 
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "infos-🗞").send(`${member} vient d'arriver ! Souhaité lui la bienvenue :wink: \n Nous sommes **{membercount}**`)
+    member.guild.channels.find("name", "infos-🗞").send(`${member} vient d'arriver ! Souhaité lui la bienvenue :wink: \n Nous sommes **${membercount}**`)
 });
 
 bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "infos-🗞").send(`${member} a quitté  **P&C Minecraft**. Nous sommes ravis de t'avoir accueilli :cry: \n Nous sommes **{membercount}**`)
+    member.guild.channels.find("name", "infos-🗞").send(`${member} a quitté  **P&C Minecraft**. Nous sommes ravis de t'avoir accueilli :cry: \n Nous sommes **${membercount}**`)
 });
 
 bot.on("guildMemberAdd", member => {
